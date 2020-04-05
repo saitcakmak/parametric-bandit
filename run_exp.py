5045e9be7a216992c0b8e3eda1a571a960f0ccff
saitcakmak <saitcakmak@outlook.com>
2020-04-05 14:50:43 -0400

    e

--- run_exp.py
+++ run_exp.py
@@ -5,16 +5,16 @@ import torch
 from experiment import single_rep
 from time import time
 
-budget_list = [5, 10, 20, 40, 60, 80, 100, 150, 200, 300, 400]
+budget_list = [5, 10, 20, 40, 60, 80, 100, 150, 200]
 rep = 15
 n = [10, 10, 10]
-obs_std = 10.
+obs_std = 4.
 num_init_samples = 1
 
 # TODO: do it so that instead of just running it again and again, we add samples to last one
 #       optimize by avoiding gp fitting with ocba runs
 
-output_file = 'std=10.pt'
+output_file = 'std=4.pt'
 try:
     output = torch.load(output_file)
     results = output['results']
