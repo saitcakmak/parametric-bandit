ac6babd03b605015fda28bdcc961d8625bc3fd85
saitcakmak <saitcakmak@outlook.com>
2020-09-10 14:21:25 -0400

    refactoring

--- aws_main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# make sure you modified the runner file accordingly
-for i in {1..10}
-do
-python -W ignore run_exp.py
-done
-
-# make sure to enter the instance id here
-aws ec2 stop-instances --instance-ids i-0a31140653de5e631
